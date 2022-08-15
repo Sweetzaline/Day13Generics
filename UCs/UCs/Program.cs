e023dc402f89307d04ae3336ee08c2d0bee21612
unknown <buragohain4@gmail.com>
2022-08-15 17:52:13 +0530

    Implemented Assignment

---
+++ UCs/UCs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 112, 334, 432, 555, 678, 800 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.PrintMaxValue();
+        }
+    }
+}
