e8f7cb0473b554b9135f4ae631ddf80873b0c0ac
unknown <buragohain4@gmail.com>
2022-08-15 17:55:08 +0530

    Class Practise

---
+++ GenricsDemoPractise/genricsdemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace genricsdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Program.toPrint( doubleArray);
+            Program.toPrint( charArray);
+        }
+
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------");
+        }
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------");
+        }
+    }
+}
